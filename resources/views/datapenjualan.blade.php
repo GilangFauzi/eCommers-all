b357457516a426afc63bf0d97a32dd4c05b0317f
irgifahlevi <irgifahlevi5@gmail.com>
2022-07-30 23:33:01 +0700

    form tambah

--- resources/views/datapenjualan.blade.php
+++ resources/views/datapenjualan.blade.php
@@ -3,6 +3,21 @@
 
 <section class="section dashboard">
     <div class="row">
+        <div class="d-sm-flex align-items-center justify-content-between border-bottom mb-4 mt-2">
+            
+            
+                <form class="search-form d-flex align-items-center justify-content-between" method="POST" action="#">
+                    <input class="me-1 mb-3" type="text" name="query" title="Enter search keyword">
+                    <button type="button" class="btn btn-outline-primary btn-sm mb-3 me-4">Cari</button>
+                </form>
+                <div>
+                    <div class="btn-wrapper">
+                    <a href="#" class="btn-sm btn btn-danger mb-3 me-2">Export PDF</a>
+                    <a href="#" class="btn-sm btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#verticalycentered">Tambah</a>
+                  </div>
+                </div>
+          </div>
+
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">Data Penjualan</h5>
@@ -60,6 +75,88 @@
 
             </div>
         </div>
+
+
+        <div class="modal fade" id="verticalycentered" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Vertically Centered</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form class="row g-3 needs-validation" novalidate>
+                        <div class="col-md-4">
+                          <label for="validationCustom01" class="form-label">First name</label>
+                          <input type="text" class="form-control" id="validationCustom01" value="John" required>
+                          <div class="valid-feedback">
+                            Looks good!
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <label for="validationCustom02" class="form-label">Last name</label>
+                          <input type="text" class="form-control" id="validationCustom02" value="Doe" required>
+                          <div class="valid-feedback">
+                            Looks good!
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <label for="validationCustomUsername" class="form-label">Username</label>
+                          <div class="input-group has-validation">
+                            <span class="input-group-text" id="inputGroupPrepend">@</span>
+                            <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+                            <div class="invalid-feedback">
+                              Please choose a username.
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <label for="validationCustom03" class="form-label">City</label>
+                          <input type="text" class="form-control" id="validationCustom03" required>
+                          <div class="invalid-feedback">
+                            Please provide a valid city.
+                          </div>
+                        </div>
+                        <div class="col-md-3">
+                          <label for="validationCustom04" class="form-label">State</label>
+                          <select class="form-select" id="validationCustom04" required>
+                            <option selected disabled value="">Choose...</option>
+                            <option>...</option>
+                          </select>
+                          <div class="invalid-feedback">
+                            Please select a valid state.
+                          </div>
+                        </div>
+                        <div class="col-md-3">
+                          <label for="validationCustom05" class="form-label">Zip</label>
+                          <input type="text" class="form-control" id="validationCustom05" required>
+                          <div class="invalid-feedback">
+                            Please provide a valid zip.
+                          </div>
+                        </div>
+                        <div class="col-12">
+                          <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                            <label class="form-check-label" for="invalidCheck">
+                              Agree to terms and conditions
+                            </label>
+                            <div class="invalid-feedback">
+                              You must agree before submitting.
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12">
+                          <button class="btn btn-primary" type="submit">Submit form</button>
+                        </div>
+                      </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div><!
     </div>
 </section>
 @endsection
