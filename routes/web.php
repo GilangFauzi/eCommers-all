8fb5818c3ed3a101959029edea17a565f17e9810
rizkyirwan <kirunpengki@gmail.com>
2022-07-30 15:08:32 +0700

    Tambah halaman penjualan cui

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PenjualanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('datapenjualan');
+    return view('index');
 });
+
+Route::get('/datapenjualan', [PenjualanController::class, 'index'])->name('datapenjualan');
